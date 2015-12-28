c8933a80160b676a687a380eb79f5d0250d44bcf
anthonybrown <tony@tonybrown.me>
2015-12-28 05:33:45 -0500

    add app already in development to repo

---
+++ app/js/controllers.js
@@ -0,0 +1,16 @@
+'use strict';
+/* Controllers */
+angular.module('myApp.controllers', [])
+	.controller('LandingPageController', [function() {
+  }])
+  .controller('WaitlistController', ['$scope', '$firebase', function($scope, $firebase) {
+  	var partiesRef = new Firebase('https://waitagain-tony.firebaseio.com/');
+  	$scope.parties = $firebase(partiesRef);
+
+  	$scope.newParty = { name: '', phone: '', size: '' };
+
+  	$scope.saveParty = function () {
+  		$scope.parties.$add($scope.newParty);
+			$scope.newParty = { name: '', phone: '', size: '' }
+  	};
+  }]);
